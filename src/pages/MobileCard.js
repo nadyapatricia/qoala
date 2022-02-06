5f7694157058aee5c9836ea76cd2b844006fbb04
Nadya Patricia <nadya.patricia@xfers.com>
2022-02-07 01:01:14 +0700

    Create mobile view

---
+++ src/pages/MobileCard.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Avatar, Card } from 'antd';
+import { EllipsisOutlined, UserOutlined } from '@ant-design/icons';
+import styled from '@emotion/styled';
+
+export default function MobileCard({ user }) {
+  const { id, title, firstName, lastName, age, email, city, posCode, state } =
+    user;
+
+  return (
+    <StyledCard
+      size='small'
+      title={
+        <>
+          <span style={{ color: '#adadad' }}>Driver ID</span>{' '}
+          <span style={{ color: '#1890FF' }}>{id}</span>
+        </>
+      }
+      extra={<EllipsisOutlined />}
+      id={id}
+    >
+      <ContentWrapper>
+        <Avatar
+          size={90}
+          icon={<UserOutlined />}
+          style={{ marginRight: '25px' }}
+        />
+        <UserBarBody>
+          <Details>
+            <Header>Name</Header>
+            <Content>{`${title} ${firstName} ${lastName}`}</Content>
+          </Details>
+          <Details>
+            <Header>Age</Header>
+            <Content>{`${age}`}</Content>
+          </Details>
+          <Details>
+            <Header>Address</Header>
+            <Content>{`${city} ${state} ${posCode}`}</Content>
+          </Details>
+          <Details>
+            <Header>Email</Header>
+            <Content>{email}</Content>
+          </Details>
+        </UserBarBody>
+      </ContentWrapper>
+    </StyledCard>
+  );
+}
+
+const UserBarBody = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentWrapper = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const Details = styled.div`
+  margin-bottom: 5px;
+  align: left;
+  &:last-child {
+    margin-bottom: -20px;
+  }
+`;
+
+const Content = styled.p`
+  height: auto;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Header = styled.p`
+  margin-bottom: 2px;
+  font-weight: 500;
+  color: #adadad;
+`;
+
+const StyledCard = styled(Card)`
+  width: 100%;
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
--- src/pages/MobileView.js
+++ src/pages/MobileView.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { DESKTOP_BREAKPOINT } from '../constants';
+import MobileCard from './MobileCard';
 
-export default function MobileView() {
-  return <div></div>;
+export default function MobileView({ isUsersLoading, users }) {
+  return (
+    <MobileCardContainer>
+      {!isUsersLoading &&
+        users.length > 0 &&
+        users.map((user) => {
+          return <MobileCard user={user} />;
+        })}
+    </MobileCardContainer>
+  );
 }
+
+const MobileCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  height: 100%;
+
+  ${DESKTOP_BREAKPOINT} {
+    display: none;
+  }
+`;
